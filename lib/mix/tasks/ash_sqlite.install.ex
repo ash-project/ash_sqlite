3d148cc73e0de4bc4d9339c211a0e7d0378aa4c7
James Harton <james@harton.nz>
2024-09-23 09:38:06 +1200

    fix(installer): use correct module name in the `DataCase` moduledocs.

--- lib/mix/tasks/ash_sqlite.install.ex
+++ lib/mix/tasks/ash_sqlite.install.ex
@@ -200,6 +200,8 @@ defmodule Mix.Tasks.AshSqlite.Install do
   end
 
   defp setup_data_case(igniter) do
+    module_name = Igniter.Code.Module.module_name(igniter, "DataCase")
+
     default_data_case_contents = ~s|
     @moduledoc """
     This module defines the setup for tests requiring
@@ -212,7 +214,7 @@ defmodule Mix.Tasks.AshSqlite.Install do
     we enable the SQL sandbox, so changes done to the database
     are reverted at the end of every test. If you are using
     PostgreSQL, you can even run database tests asynchronously
-    by setting `use AshHq.DataCase, async: true`, although
+    by setting `use #{inspect(module_name)}, async: true`, although
     this option is not recommended for other databases.
     """
 
@@ -235,8 +237,6 @@ defmodule Mix.Tasks.AshSqlite.Install do
       :ok
     end
     |
-
-    module_name = Igniter.Code.Module.module_name(igniter, "DataCase")
 
     igniter
     |> Igniter.Code.Module.find_and_update_or_create_module(
