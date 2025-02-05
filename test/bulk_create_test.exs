2045a0ea81974d5be338b1708fea6f227401160f
Zach Daniel <zach@zachdaniel.dev>
2025-02-05 15:35:14 -0500

    test: fix tests to account for ash bulk action fix

--- test/bulk_create_test.exs
+++ test/bulk_create_test.exs
@@ -85,7 +85,7 @@ defmodule AshSqlite.BulkCreateTest do
   describe "validation errors" do
     test "skips invalid by default" do
       assert %{records: [_], errors: [_]} =
-               Ash.bulk_create!([%{title: "fred"}, %{title: "not allowed"}], Post, :create,
+               Ash.bulk_create([%{title: "fred"}, %{title: "not allowed"}], Post, :create,
                  return_records?: true,
                  return_errors?: true
                )
