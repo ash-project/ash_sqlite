e792c8d4417388c51e0b3f0aa6c5bf7ce201824f
James Harton <james@harton.nz>
2025-10-15 08:40:58 +1300

    chore: Fix REUSE copyright attributions

--- test/support/relationships/comments_containing_title.ex
+++ test/support/relationships/comments_containing_title.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 Zach Daniel
+# SPDX-FileCopyrightText: 2023 ash_sqlite contributors <https://github.com/ash-project/ash_sqlite/graphs.contributors>
 #
 # SPDX-License-Identifier: MIT
 
